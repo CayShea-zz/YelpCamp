d826b9b1867e5d063b30ff162526fbe7f82c94d3
Caylee-Shea Harrington <cayleesheaharrington@gmail.com>
2018-09-20 22:27:59 +0000

    updated code to display google maps and request location from user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,15 @@ var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
 var middleware = require("../middleware");
+var NodeGeocoder = require("node-geocoder");
+
+var options = {
+    provier: "google",
+    httpAdapter: "https",
+    apiKey: process.env.GEOCODER_API_KEY,
+    formatter: null
+};
+var geocoder = NodeGeocoder(options);
 
 //INDEX ROUTE
 router.get("/campgrounds", function(req,res){
@@ -28,7 +37,17 @@ router.post("/campgrounds", middleware.isLoggedIn, function(req,res){
        id: req.user._id,
        username: req.user.username
    };
-  
+  //Google maps
+   geocoder.geocode(req.body.location, function (err, data) {
+    if (err || !data.length) {
+      req.flash('error', 'Invalid address');
+      return res.redirect('back');
+    }
+    var lat = data[0].latitude;
+    var lng = data[0].longitude;
+    var location = data[0].formattedAddress;
+    
+ 
   //create var for the 'newCampground'
    var newCampground = {name: name, price: price, image:image, description: description, author: author};
   
@@ -74,6 +93,16 @@ router.get("/campgrounds/:id/edit", middleware.checkCampAuthorization, function(
 
 //SHOW Route after EDIT with 'put'
 router.put("/campgrounds/:id", middleware.checkCampAuthorization, function(req, res){
+    //GOOGLE map
+    geocoder.geocode(req.body.location, function (err, data) {
+    if (err || !data.length) {
+      req.flash('error', 'Invalid address');
+      return res.redirect('back');
+    }
+    req.body.campground.lat = data[0].latitude;
+    req.body.campground.lng = data[0].longitude;
+    req.body.campground.location = data[0].formattedAddress;
+    
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
        if(err){
